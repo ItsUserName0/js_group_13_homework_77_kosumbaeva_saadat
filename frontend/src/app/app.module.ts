080af85cdb305a6ef19e9f69a905a28f08cd93ce
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2022-02-04 12:00:56 +0600

    Created basic app structure.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ThreadComponent } from './pages/thread/thread.component';
+import { CardsComponent } from './pages/thread/cards/cards.component';
+import { CardComponent } from './pages/thread/cards/card/card.component';
+import { NewCardComponent } from './pages/thread/new-card/new-card.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FileInputComponent } from './ui/file-input/file-input.component';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThreadComponent,
+    CardsComponent,
+    CardComponent,
+    NewCardComponent,
+    FileInputComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexModule,
+    FormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
